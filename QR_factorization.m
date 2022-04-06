2f9b03e8103f8aa02527d613ade59a8f59ccd9f5
huawenbo <86921617+huawenbo@users.noreply.github.com>
2022-04-06 15:11:35 +0800

    Update QR_factorization.m

--- QR_factorization.m
+++ QR_factorization.m
@@ -15,7 +15,7 @@ Q = zeros(m, n);
 R = zeros(m, n);
 for i = 1:n
     if i == 1    
-        p(:, i) = A(:, i);
+        p(:, i) = A(:, i)
         Q(:, i) = p(:, i)/norm(p(:, i), 2);
         R(1, 1) = norm(p(:, i), 2);  
     else
